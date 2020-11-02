526f0d04613a0c8d9d56da455a978b22a8f592cb
sengokyu <sengokyu+gh@gmail.com>
2020-11-02 13:01:45 +0900

    Complete

--- ischema2tsv.py
+++ ischema2tsv.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+from os import stat
+import sys
 import argparse
 import getpass
+import re
 import mysql.connector
 from contextlib import contextmanager
 
@@ -40,23 +43,12 @@ class ColumnDao:
         query = """
         SELECT
           C.COLUMN_NAME
-        , C.ORDINAL_NUMBER
         , C.COLUMN_DEFAULT
         , C.IS_NULLABLE
         , C.COLUMN_TYPE
-        , T.CONSTRAINT_TYPE
-        , K.REFERENCED_TABLE_NAME
-        , K.REFERENCED_COLUMN_NAME
           FROM information_schema.COLUMNS C
-          LEFT JOIN information_schema.TABLE_CONSTRAINTS T
-            ON C.TABLE_SCHEMA = T.TABLE_SCHEMA
-           AND C.TABLE_NAME = T.TABLE_NAME
-          LEFT JOIN information_schema.KEY_COLUMN_USAGE K
-            ON C.TABLE_SCHEMA = K.TABLE_SCHEMA
-           AND C.TABLE_NAME = K.TABLE_NAME
-           AND C.COLUMN_NAME = K.COLUMN_NAME
-         WHERE TABLE_SCHEMA=%s
-           AND TABLE_NAME=%s
+         WHERE C.TABLE_SCHEMA=%s
+           AND C.TABLE_NAME=%s
          ORDER BY C.ORDINAL_POSITION
         """
         cursor = cnx.cursor()
@@ -68,40 +60,62 @@ class ColumnDao:
             cursor.close()
 
 
-class TableConstraintDao:
+class KeyColumnUsageDao:
     @staticmethod
     @contextmanager
-    def find_by_table_schema_and_table_name(cnx, table_schema: str, table_name: str):
+    def find_by_table_schema_and_table_name_and_column_name(cnx, table_schema: str, table_name: str, column_name: str):
         query = """
-        SELECT T.CONSTRAINT_TYPE, C.COLUMN_NAME, C.REFERENCED_TABLE_NAME, C.REFERENCED_COLUMN_NAME
-          FROM information_schema.TABLE_CONSTRAINTS T
-         INNER JOIN information_schema.KEY_COLUMN_USAGE C
-            ON T.CONSTRAINT_CATALOG = C.CONSTRAINT_CATALOG
-           AND T.CONSTRAINT_SCHEMA = C.CONSTRAINT_SCHEMA
-           AND T.CONSTRAINT_NAME = C.CONSTRAINT_NAME
-           AND T.TABLE_SCHEMA = C.TABLE_SCHEMA
-           AND T.TABLE_NAME = C.TABLE_NAME
-         WHERE TABLE_SCHEMA=%s
-           AND TABLE_NAME=%s
-         ORDER BY ORDINAL_POSITION
+        SELECT T.CONSTRAINT_TYPE, K.REFERENCED_TABLE_NAME, K.REFERENCED_COLUMN_NAME
+          FROM information_schema.KEY_COLUMN_USAGE K
+          LEFT JOIN information_schema.TABLE_CONSTRAINTS T
+            ON K.TABLE_SCHEMA = T.TABLE_SCHEMA
+           AND K.TABLE_NAME = T.TABLE_NAME
+         WHERE K.TABLE_SCHEMA=%s
+           AND K.TABLE_NAME=%s
+           AND K.COLUMN_NAME=%s
         """
         cursor = cnx.cursor()
 
         try:
-            cursor.execute(query, [table_schema, table_name])
+            cursor.execute(query, [table_schema, table_name, column_name])
             yield cursor
         finally:
             cursor.close()
 
 
 class ColumnDef:
-    def __init__(self, name: str):
-        self.name = name
+    column_type_pattern = re.compile('^(.+)\((.*)\)$')
 
+    def __init__(self, table_name, column_name, column_type, is_nullable, column_default):
+        self.table_name = table_name
+        self.column_name = column_name
+        self.set_column_type(column_type)
+        self.is_nullable = is_nullable
+        self.columun_default = column_default if column_default is not None else ''
+        self.is_primary_key = ''
+        self.foreign_key = ''
 
-class TableDef:
-    def __init__(self, name: str):
-        self.name = name
+    def set_column_type(self, column_type):
+        m = ColumnDef.column_type_pattern.match(column_type)
+        if m:
+            self.column_type = m.group(1)
+            self.column_size = m.group(2)
+        else:
+            self.column_type = column_type
+            self.column_size = ''
+
+    def set_constraint(self, constraint_type, table_name, column_name):
+        if constraint_type == 'PRIMARY KEY':
+            self.is_primary_key = 'YES'
+        elif constraint_type == 'FOREIGN KEY':
+            self.foreign_key = '{}.{}'.format(table_name, column_name)
+
+    def to_tuple(self):
+        return (self.table_name, self.column_name,
+                self.column_type,
+                self.column_size,
+                self.is_nullable, self.columun_default, self.is_primary_key,
+                self.foreign_key)
 
 
 def create_argparser():
@@ -128,18 +142,33 @@ def main():
         'port': args.port,
         'database': args.database
     }
+    table_schema = args.database
+
+    print('\t'.join(('Table', 'Column', 'Type', 'Size',
+                     'Nullable', 'Default', 'PKEY', 'FKEY')))
 
     with create_connection(config) as cnx:
-        with TableDao.find_by_table_schema(cnx, args.database) as cursor:
+        with TableDao.find_by_table_schema(cnx, table_schema) as cursor:
             tables = cursor.fetchall()
 
-            for table_name in tables:
-                print(table_name)
+        for tables_row in tables:
+            table_name = tables_row[0]
 
-                with ColumnDao.find_by_table_schema_and_table_name(cnx, args.database, 'titles') as cursor:
-                    for (column_name, column_default, is_nullable, column_type) in cursor:
-                        print(column_name, column_default,
-                              is_nullable, column_type)
+            with ColumnDao.find_by_table_schema_and_table_name(cnx, table_schema, table_name) as cursor:
+                columns = cursor.fetchall()
+
+            for (column_name, column_default, is_nullable, column_type) in columns:
+                output = ColumnDef(table_name, column_name,
+                                   column_type, is_nullable, column_default)
+
+                with KeyColumnUsageDao.find_by_table_schema_and_table_name_and_column_name(cnx, table_schema, table_name, column_name) as cursor:
+                    column_usages = cursor.fetchall()
+
+                for (constraint_type, referenced_table_name, referenced_column_name) in column_usages:
+                    output.set_constraint(
+                        constraint_type, referenced_table_name, referenced_column_name)
+
+                print('\t'.join(output.to_tuple()))
 
 
 if __name__ == '__main__':
